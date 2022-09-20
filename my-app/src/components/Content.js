b42a633d906a2826a7b64d48fdff5eb93fcf3fb8
Diogo Carvalho <dcar84@gmail.com>
2022-09-20 21:09:41 +0000

    Simulate API call using component lifecycle

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -2,22 +2,36 @@ import React, { Component } from 'react'
 import css from './css/Content.module.css'
 import {savedPosts} from '../posts.json'
 import PostItem from './PostItem'
+import Loader from './Loader'
 
 class Content extends Component {
     constructor(props) {
       super(props)
     
       this.state = {
-        
+        isLoaded: false,
       }
     }
+
+    componentDidMount(){
+      setTimeout(() => {
+        this.setState({
+          isLoaded: true,
+        })
+      }, 2000)
+    }
+    
   render() {
     return (
       <div>
         <div className={css.TitleBar}>My Photos</div>
-        <div className={css.SearchResults}>{savedPosts.map((post) => {
+        <div className={css.SearchResults}>
+          {this.state.isLoaded ? 
+          (savedPosts.map((post) => {
             return <div key={post.title}><PostItem post={post} className={css.SearchItem}/></div>
-        })}</div>
+          })) : 
+          (<Loader />)}
+        </div>
     </div>
     )
   }
