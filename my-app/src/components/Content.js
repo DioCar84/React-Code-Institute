891fc715b3611a8b7d1b613e832487aa9127ebb2
Diogo Carvalho <dcar84@gmail.com>
2022-11-06 16:28:58 +0000

    Add functionality to filter posts

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -10,27 +10,46 @@ class Content extends Component {
 
     this.state = {
       isLoaded: false,
+      posts: [],
     };
   }
 
   componentDidMount(prevState) {
     setTimeout(() => {
       this.setState(
-        {isLoaded: true}) 
+        {isLoaded: true,
+        posts: savedPosts}) 
     }, 2000)
+  }
+
+  handleNameChange = (e) => {
+    const {value} = e.target;
+    let filteredPosts = savedPosts.filter(post => {
+      return post.name.toLocaleLowerCase().includes(value.toLocaleLowerCase())
+    })
+
+    this.setState({
+      posts: filteredPosts,
+    })
   }
 
   render() {
     return (
-      <div>
+      <div className={css.Content}>
         <div className={css.TitleBar}>
           <h1>My Photos</h1>
+          <form>
+            <label htmlFor="searchInput">Search:</label>
+            <input onChange={e => this.handleNameChange(e)} type='search' id="searchInput"/>
+            <h4>Posts found: {this.state.posts.length}</h4>
+          </form>
+         
         </div>
         <div className={css.SearchResults}>
           {!this.state.isLoaded ? (
             <Loader />
           ) : (
-            <PostItem savedPosts={savedPosts} />
+            <PostItem savedPosts={this.state.posts} />
           )}
         </div>
       </div>
